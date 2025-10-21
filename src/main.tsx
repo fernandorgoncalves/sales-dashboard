fa2eecb1fafe479c28c555bd39ad901a2fa5572c
Fernando <fernandorgoncalves@hotmail.com>
2025-10-21 08:13:59 -0300

    Feat: Create Themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyle, lightTheme } from './Styles/'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={lightTheme}>
+    <GlobalStyle/>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
